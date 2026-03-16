60ebe3a74aac45d2c14a8c0e1d1a1b931de73b72
unknown <guilherme.pereira1607@gmail.com>
2026-03-16 08:46:24 -0300

    debug database connection

--- database/db.py
+++ database/db.py
@@ -1,20 +1,21 @@
-"""
-Conexão com PostgreSQL (Supabase)
-"""
-
 import psycopg2
 import streamlit as st
 
 
 def get_connection():
 
-    conn = psycopg2.connect(
-        host=st.secrets["DB_HOST"],
-        database="postgres",
-        user="postgres",
-        password=st.secrets["DB_PASSWORD"],
-        port=5432,
-        sslmode="require"
-    )
+    try:
+        conn = psycopg2.connect(
+            host=st.secrets["DB_HOST"],
+            database="postgres",
+            user="postgres",
+            password=st.secrets["DB_PASSWORD"],
+            port=5432,
+            sslmode="require"
+        )
 
-    return conn
+        return conn
+
+    except Exception as e:
+        st.error(f"Erro de conexão: {e}")
+        raise e
